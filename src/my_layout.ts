b3f2fc3f633eddbea826a3dddf8d06de1beeba65
Slaus <SlavMFM@gmail.com>
2021-05-15 13:00:17 +0400

    better tree drawing

--- src/my_layout.ts
+++ src/my_layout.ts
@@ -1,20 +1,20 @@
 import * as svg from '@svgdotjs/svg.js'
 import Streams from '@slaus/simple_streams/lib/space'
 import { random_int } from './utils'
+const randomColor = require('randomcolor')
 
-/** Which direction tree is growing.*/
-const DIRECTION = Math.PI / 4
-const BRANCH = Math.PI / 8
-const NODE_RADIUS = 28
-const EDGE_LENGTH = 64
+const NODE_RADIUS = 22
+const EDGE_LENGTH = 90
 
 class Node {
     value : number
+    color : string
     left  : Tree = new Tree
     right : Tree = new Tree
 
-    constructor( value : number ) {
+    constructor( value : number, color : string ) {
         this.value = value
+        this.color = color
     }
 }
 class Tree {
@@ -43,58 +43,89 @@ export default function( streams : Streams ) {
     const bst = new Tree
 
     const canvas = svg.SVG().addTo('#chartdiv').size('100%', '100%')
-    const nodes = canvas.group()
 
-    function draw() {
-        nodes.clear()
+    streams.s( 'draw' ).on( () => {
+        canvas.clear()
+
+        const bbox = document.getElementById('chartdiv').getBoundingClientRect()
+
         draw_node(
             canvas,
-            200,
-            200,
-            DIRECTION,
+            bbox.width / 2,
+            bbox.height / 2,
             bst.node,
         )
-    }
-    streams.s( 'resize' ).on( draw )
-    draw()
-
-    streams.s( 'spacebar' ).on( () => {
-        insert( bst, new Node( random_int( -100, 100 ) ) )
-        draw()
     })
+    streams.s( 'resize' ).to( 'draw' )
+    streams.s( 'draw' ).next()
+
+    streams.s( 'spacebar' )
+        .do( () => {
+            insert(
+                bst,
+                new Node(
+                    random_int( -100, 100 ),
+                    randomColor(),
+                )
+            )
+        })
+        .to( 'draw' )
 }
 
 function draw_node(
     canvas : svg.Svg,
     x : number,
     y : number,
-    direction : number,
     node : Node | undefined,
+    direction : number = Math.PI / 2,
+    depth = 0,
+    range = Math.PI,
 ) {
     if ( ! node )
         return
     
     const view = canvas.group()
-    view.circle( NODE_RADIUS * 2 ).fill( '#5144ff' ).attr( 'shape-rendering', 'optimizeQuality' ).attr( 'anchor', 'middle' )
-    view.text( node.value.toString() ).fill( '#00291d' ).attr( 'anchor', 'middle' ).attr( 'font-size', NODE_RADIUS ).center( NODE_RADIUS, NODE_RADIUS )
+    view
+        .circle( NODE_RADIUS * 2 )
+        .fill( node.color )
+        .attr( 'shape-rendering', 'geometricPrecision' )
+        .attr( 'anchor', 'middle' )
+    view
+        .text( node.value.toString() )
+        .fill( '#00291d' )
+        .attr( 'anchor', 'middle' )
+        .attr( 'font-size', NODE_RADIUS )
+        .attr({
+            'stroke' : '#0049bf',
+            'stroke-width' : '1px',
+            'stroke-opacity' : '1',
+        })
+        .center( NODE_RADIUS, NODE_RADIUS )
 
     view.translate( x, y )
-    console.log( 'x:', x, 'y:', y )
     
+    const next_range = range / 2 + Math.PI * ( 1 - Math.pow( 0.97, depth ) )
+    const edge_length = EDGE_LENGTH * Math.pow( 0.97, depth )
     //left:
+    const left_dir = direction - next_range
     draw_node(
         canvas,
-        x + Math.sin( direction - BRANCH ) * EDGE_LENGTH,
-        y + Math.cos( direction - BRANCH ) * EDGE_LENGTH,
-        direction - BRANCH,
+        x + Math.sin( left_dir ) * edge_length,
+        y + Math.cos( left_dir ) * edge_length,
         node.left.node,
+        left_dir,
+        depth + 1,
+        next_range,
     )
     //right:
+    const right_dir = direction + next_range
     draw_node(
         canvas,
-        x + Math.sin( direction + BRANCH ) * EDGE_LENGTH,
-        y + Math.cos( direction + BRANCH ) * EDGE_LENGTH,
-        direction + BRANCH,
+        x + Math.sin( right_dir ) * edge_length,
+        y + Math.cos( right_dir ) * edge_length,
         node.right.node,
+        right_dir,
+        depth + 1,
+        next_range,
     )
 }
