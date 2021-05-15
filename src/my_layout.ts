d2d7eacb751a1498201fa300737d56c87be42dbd
Slaus <SlavMFM@gmail.com>
2021-05-15 11:49:17 +0400

    tree insertion with futile attempt to position nodes in a tree-like structure

--- src/my_layout.ts
+++ src/my_layout.ts
@@ -3,47 +3,98 @@ import Streams from '@slaus/simple_streams/lib/space'
 import { random_int } from './utils'
 
 /** Which direction tree is growing.*/
-const DIR = Math.PI / 4
-const NODE_RADIUS = 40
-const EDGE_LENGTH = 40
+const DIRECTION = Math.PI / 4
+const BRANCH = Math.PI / 8
+const NODE_RADIUS = 28
+const EDGE_LENGTH = 64
 
 class Node {
     value : number
-    left  : Node | undefined = undefined
-    right : Node | undefined = undefined
+    left  : Tree = new Tree
+    right : Tree = new Tree
 
     constructor( value : number ) {
         this.value = value
     }
 }
+class Tree {
+    node : Node | undefined = undefined
+}
+
+function insert( tree : Tree, new_one : Node ) {
+    if ( ! tree.node ) {
+        tree.node = new_one
+        return
+    }
+
+    //value already present in the tree:
+    if ( new_one.value == tree.node.value )
+        return
+    
+    if ( new_one.value < tree.node.value ) {
+        insert( tree.node.left, new_one )
+        return
+    }
+
+    insert( tree.node.right, new_one )
+}
 
 export default function( streams : Streams ) {
-    let root : Node | undefined = undefined
+    const bst = new Tree
 
     const canvas = svg.SVG().addTo('#chartdiv').size('100%', '100%')
     const nodes = canvas.group()
 
     function draw() {
         nodes.clear()
-        draw_node( canvas, root )
+        draw_node(
+            canvas,
+            200,
+            200,
+            DIRECTION,
+            bst.node,
+        )
     }
     streams.s( 'resize' ).on( draw )
     draw()
 
     streams.s( 'spacebar' ).on( () => {
-        root = new Node( 42 )
+        insert( bst, new Node( random_int( -100, 100 ) ) )
         draw()
     })
 }
 
-function draw_node( canvas : svg.Svg, node : Node | undefined ) {
+function draw_node(
+    canvas : svg.Svg,
+    x : number,
+    y : number,
+    direction : number,
+    node : Node | undefined,
+) {
     if ( ! node )
         return
     
     const view = canvas.group()
     view.circle( NODE_RADIUS * 2 ).fill( '#5144ff' ).attr( 'shape-rendering', 'optimizeQuality' ).attr( 'anchor', 'middle' )
-    view.text( '42' ).fill( '#00291d' ).attr( 'anchor', 'middle' ).attr( 'font-size', 50 ).center( NODE_RADIUS, NODE_RADIUS )
+    view.text( node.value.toString() ).fill( '#00291d' ).attr( 'anchor', 'middle' ).attr( 'font-size', NODE_RADIUS ).center( NODE_RADIUS, NODE_RADIUS )
+
+    view.translate( x, y )
+    console.log( 'x:', x, 'y:', y )
     
-    draw_node( canvas, node.left )
-    draw_node( canvas, node.right )
+    //left:
+    draw_node(
+        canvas,
+        x + Math.sin( direction - BRANCH ) * EDGE_LENGTH,
+        y + Math.cos( direction - BRANCH ) * EDGE_LENGTH,
+        direction - BRANCH,
+        node.left.node,
+    )
+    //right:
+    draw_node(
+        canvas,
+        x + Math.sin( direction + BRANCH ) * EDGE_LENGTH,
+        y + Math.cos( direction + BRANCH ) * EDGE_LENGTH,
+        direction + BRANCH,
+        node.right.node,
+    )
 }
